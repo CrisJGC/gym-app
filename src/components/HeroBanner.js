b317919a8028ec13317151c437a89a32ba3db43c
Cristian Gallegos <jacob.gallegos.c@gmail.com>
2022-09-14 12:10:43 -0500

    banner updated

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
-import HeroBannerImage from '../assets/images/banner.png';
+import HeroBannerImage from '../assets/images/banner-o.jpg';
 
 const HeroBanner = () => (
   <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
@@ -22,7 +22,7 @@ const HeroBanner = () => (
     <Typography fontWeight={600} color="#FF2625" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
       Exercise
     </Typography>
-    <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" />
+    <img src={HeroBannerImage} style={{marginRight: '-40px'}} alt="hero-banner" className="hero-banner-img" />
   </Box>
 );
 
